f408a1d2f388f08da17c8c30dea6be5c9d3c7272
MSVVSSAINADH <macharlasainadh2506@gmail.com>
2025-10-16 13:41:20 +0530

    V1.0.1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './travelmanagement', // 👈 THIS IS THE FIX
+  base: '/travelmanagement/',
 })
